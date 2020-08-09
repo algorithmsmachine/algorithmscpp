70c685694082d18c059a09b9b2455ea2d2c79628
altanai <tara181989@gmail.com>
2020-08-09 10:56:36 +0530

    first push

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+//
+// Created by altanai on 09/08/20.
+//
+
+#include <iostream>
+//using namespace std;
+
+int main() {
+    cout << "Hello World";
+}
---
+++ test.cpp
@@ -0,0 +1,8 @@
+//
+// Created by altanai on 09/08/20.
+//
+
+#include "test.h"
+#include <iostream>
+using namespace std;
+cout << "Hello World";
